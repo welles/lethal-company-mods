4370037073f346b534f9434823f15a1fd5f3252b
Nico Welles <nico@welles.io>
2024-01-07 13:29:27 +0100

    Download update script file before executing it

--- Update-Mods.bat
+++ Update-Mods.bat
@@ -1,5 +1,7 @@
 @echo off
 
+powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/welles/lethal-company-mods/main/Update-Mods.ps1' -OutFile '%~dp0\Update-Mods.ps1'"
+
 powershell.exe -ExecutionPolicy Bypass -NoProfile -File "%~dp0\Update-Mods.ps1"
 
 pause
